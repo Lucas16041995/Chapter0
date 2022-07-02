0459fc7d299887f8ebd544299c992f207eff47ab
Lucas16041995 <104994847+Lucas16041995@users.noreply.github.com>
2022-07-02 22:57:05 +0800

    Add files via upload

---
+++ src/VariableAssignment.java
@@ -0,0 +1,8 @@
+public class VariableAssignment {
+    public static void main(String[] args) {
+        int x = 5; // 設定 variable x data type => Integer(整數)
+        //x = x + 1;
+        x =+ 1; //語法糖 syntax sugar
+        System.out.println(x);
+    }
+}
